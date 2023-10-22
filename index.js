6766344784f583d30b740b19a2889bf4af9b8ce5
KAILASHCOOL <133621520+KAILASHCOOL@users.noreply.github.com>
2023-10-22 17:15:19 +0530

    weather search

---
+++ index.js
@@ -0,0 +1,31 @@
+const apikey="29f0c14e93ce78db087ec685d0e75be1"
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const searchBox=document.querySelector(".search input");
+    const searchBtn=document.querySelector(".search button");
+    const weatherIcon=document.querySelector(".weatherIcon")
+
+
+    async function checkweather(city){
+    const response=await fetch(apiurl+city+`&appid=${apikey}`);
+        var data=await response.json()
+        console.log(data);
+        document.querySelector(".city").innerHTML=data.name;
+        document.querySelector(".temp").innerHTML=Math.floor (data.main
+        .temp)+"°C";
+        document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+        document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+        console.log(data.weather[0].main)
+        if (data.weather[0].main=="Clouds"){
+            weatherIcon.src="clouds.png"
+        }
+        else if (data.weather[0].main=="Clear"){
+            weatherIcon.src="sunny.png"
+        }
+        else if (data.weather[0].main=="rain"){
+            weatherIcon.src="rain.png"
+        }
+
+    }
+    searchBtn.addEventListener("click",()=>{
+        checkweather(searchBox.value);}
+        )
